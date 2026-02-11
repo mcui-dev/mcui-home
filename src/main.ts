ba0873a7b0c065e727170c218d9c40aa686feb5c
mcui <741789633@qq.com>
2026-02-11 11:29:49 +0800

    add aliyun monitor

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+const script = document.createElement('script')
+script.src = 'https://rjsperf.myalicdn.com/sdk.js'
+script.async = true
+document.head.appendChild(script)
+
 createApp(App).mount('#app')
